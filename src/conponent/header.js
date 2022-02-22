9e4efb03c023bb41c8528dc83c24904cbba1d92f
Chi Dung <Dungnc0402@gmail.com>
2022-02-22 15:32:52 +0700

    update

--- src/conponent/header.js
+++ src/conponent/header.js
@@ -14,8 +14,17 @@ const Header = {
                         <a href="#"><i class="ri-mail-line"></i> Dungnc@gmail.com</a>
                     </div>
                     <div>
-                </div>
+                        
+                        <input type="text" id="text-search" name="" value="" class="caret-blue-500 text-black">
+                        <button type="button" id="btn-search">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                            </svg>  
+                        </button> 
+                    </div>
+                    
                 <div>
+                
                     
                 <div  class="float-right login">
                     <a href="/#/signin">Đăng Nhập</a> | 
@@ -55,6 +64,14 @@ const Header = {
             })            
             
         }
+    },
+    afterRender() {
+        const btnSearch = document.querySelector("#btn-search");
+        btnSearch.addEventListener("click", (e) => {
+            e.preventDefault();
+            const textVal = document.querySelector("#text-search").value;
+            console.log(textVal);
+        }) 
     }
 }
 
